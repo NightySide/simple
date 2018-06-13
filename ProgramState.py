17f2d963062123b88adfeb3ed3b344afb0b3dc12
NightySide <nightyside@gmail.com>
2018-06-13 14:54:49 +0200

    Buffer improvement and restructuration
    Added ProgramState's "b" function to help accessing the buffer.
    
    Separated "subjects" (or module in python) in a subfolder

--- ProgramState.py
+++ ProgramState.py
@@ -3,7 +3,7 @@ import importlib
 
 def use(*name, p): #import a module, and load references to use it
     #print(name)
-    new_module = importlib.import_module(name[0])
+    new_module = importlib.import_module("subjects."+name[0])
     p.functions.update(new_module.functions)
     p.generate_aliases(new_module.functions)
     #print("Now available :", str(p.functions))
@@ -22,7 +22,8 @@ class ProgramState:
     def load(self, filename):
         self.program = funcs.readFile(filename)
         self.line = 0
-
+    def b(self, value):
+        self.buffer = value
     def __repr__(self):
         n = "\n"
         r = "----Program State----\n"
---
+++ subjects/Random.py
@@ -0,0 +1,5 @@
+from random import randrange
+
+rand = lambda a, b, p: p.b(randrange(int(a),int(b)))
+
+functions = {"randrange":rand}
